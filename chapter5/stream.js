54878e5512cb31ce1e9d804ee5784b7bc54ba106
madhukar <phatak.dev@gmail.com>
2014-07-01 16:26:16 +0530

    infinite streams added

--- chapter5/stream.js
+++ chapter5/stream.js
@@ -43,8 +43,43 @@
  		return this.foldRight(new Empty(),(function(value,t){
            if(fn.call(context,value)) return new Cons(value,t); else return t();
  		}))
+ 	},
+ 	exists : function(fn,context) {
+ 		return this.foldRight(true,function(value,t){ 		  
+ 		  return fn.call(context,value) || t();
+ 		});
+ 	},
+ 	forAll : function(fn,context){
+ 	  return this.foldRight(true,function(value,t){ 		  
+ 		  return fn.call(context,value) && t();
+ 		});	
+ 	},
+ 	zip : function(stream) {
+ 		if(this instanceof Empty || stream instanceof Empty) return new Empty();
+ 		var h1 = this.h;
+ 		var h2 = stream.h;
+ 		var t1 = this.t;
+ 		var t2 = stream.t;        
+ 		return new Cons({"_1":h1(),"_2":h2()},function(){return t1().zip(t2());});
+ 	},
+ 	unfold : function(initialValue,fn,context) {
+ 		
  	}
 
+
+ }
+
+ Stream.constant = function(value) {
+ 		return new Cons(value,function(){return Stream.constant(value);});
+ }
+ Stream.from = function(startValue) {
+ 	return new Cons(startValue,function(){return Stream.from(startValue+1)});
+ }
+ Stream.fibs = function() {
+ 	function fibGen(first,second) {
+ 		return new Cons(first,function(){ return fibGen(second,first+second);});
+ 	}
+ 	return fibGen(0,1);
  }
 
 
@@ -67,6 +102,7 @@
     	this.t = function() { return returnValue;}
     	return returnValue;
     }
+
  }
  Cons.prototype = new Stream();
 
@@ -94,5 +130,33 @@ println(mapped.toList());
 println(mapAgain.toList());
 println(take2.toList());
 println(filtered.toList());
+
+
+//infinite streams 
+var makeOnes = function() {
+   return new Cons( 1, makeOnes);    
+};
+println(makeOnes().take(10).toList());
+println(makeOnes().map(function(value){return value+1}).take(7).toList());
+
+
+//use constant
+println(Stream.constant(5).map(function(value){return value*5;}).take(7).toList());
+println(Stream.from(1).take(20).toList());
+
+//Fibonacci numbers
+println(Stream.fibs().take(10).toList());
+//check whether given number is there
+println(Stream.fibs().exists(function(value){ return value==34;}));
+println(Stream.fibs().forAll(function(value){ return value%2==0;}));
+
+var zipped = makeOnes().zip(Stream.fibs());
+println(zipped.take(5).toList());
+
+
+
+
+
+
 
 })();	
